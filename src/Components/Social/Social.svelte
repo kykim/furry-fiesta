f287117bbe3c4619ec910f020b6cdaec089c3b71
Kevin Y Kim <kykimnyc@gmail.com>
2021-02-17 13:28:31 -0500

    have social links point home for now

--- src/Components/Social/Social.svelte
+++ src/Components/Social/Social.svelte
@@ -2,16 +2,14 @@
   export let socialData = {};
   const { IMAGES_LIST, HEADING } = socialData;
 </script>
-<!------------------------------------------->
-<!----------------MARKUP----------------------->
-<!------------------------------------------->
+
 <section id="social-media" class="section grey-bgcolor">
   <div class="container text-center">
     <h2 class="title text-center">{HEADING}</h2>
     <div class="social-icons section-body">
       {#each IMAGES_LIST as list}
         <a
-          href="https://www.linkedin.com/in/nikhil-karkra-73a15319/"
+          href="https://fuzzy-fiesta.onrender.com/"
           target="_blank">
           <img src={list} alt="Social media {list}" />
         </a>
